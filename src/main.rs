5887c5373b24cc828a2cca7604bb14186c950250
Udbhav Prasad <udbhav523@gmail.com>
2025-03-03 10:06:59 -0800

    Add label to Value

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::ops;
 struct Value<'a> {
     data: f64,
     deps: Vec<&'a Value<'a>>,
+    label: &'a str,
     op: char,
 }
 
@@ -20,6 +21,7 @@ impl<'a> ops::Add<&'a Value<'a>> for &'a Value<'a> {
         Value {
             data: self.data + other.data,
             deps: vec![self, other],
+            label: "",
             op: '+',
         }
     }
@@ -32,6 +34,7 @@ impl<'a> ops::Mul<&'a Value<'a>> for &'a Value<'a> {
         Value {
             data: self.data * other.data,
             deps: vec![self, other],
+            label: "",
             op: '*',
         }
     }
@@ -40,9 +43,9 @@ impl<'a> ops::Mul<&'a Value<'a>> for &'a Value<'a> {
 impl<'a> Value<'a> {
     fn fmt_helper(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result {
         let data = if self.op == '\0' {
-            format!("({})", self.data)
+            format!("({}={})", self.label, self.data)
         } else {
-            format!("({}, '{}')", self.data, self.op)
+            format!("({}={}, '{}')", self.label, self.data, self.op)
         };
         if indent == 0 {
             writeln!(f, "{}", data)?;
@@ -56,22 +59,35 @@ impl<'a> Value<'a> {
         Ok(())
     }
 
-    fn new(data: f64) -> Value<'a> {
+    fn set_label(&mut self, label: &'a str) {
+        self.label = label;
+    }
+
+    fn new(data: f64, label: &'a str) -> Value<'a> {
         Value {
             data: data,
             deps: Vec::new(),
+            label: label,
             op: '\0',
         }
     }
 }
 
 fn main() {
-    let a = Value::new(2.0);
-    let b = Value::new(-3.0);
-    let c = Value::new(10.0);
+    let a = Value::new(2.0, "a");
+    let b = Value::new(-3.0, "b");
+    let c = Value::new(10.0, "c");
 
-    let e = &a * &b;
-    let d = &e + &c;
+    let mut e = &a * &b;
+    e.set_label("e");
 
-    println!("{}", d);
+    let mut d = &e + &c;
+    d.set_label("d");
+
+    let f = Value::new(-2.0, "f");
+
+    let mut l = &d * &f;
+    l.set_label("L");
+
+    println!("{}", l);
 }
